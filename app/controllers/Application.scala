1ec2512b43ca6bb66e9a6bada55abc2a14b8e35a
azhar <azhar.amirr@gmail.com>
2015-11-05 14:03:24 +0700

    refactor login simple

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Redirect(routes.UserManagement.loginView)
+    Redirect(routes.UserArea.loginView)
   }
 
 }
--- app/controllers/UserArea.scala
+++ app/controllers/UserArea.scala
@@ -2,6 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scala.concurrent.Future
+import models._
 
 class UserArea extends Controller {
 
@@ -12,5 +19,38 @@ class UserArea extends Controller {
       Unauthorized("Not connected")
     }
   }
-}
 
+  def loginView = Action { request => 
+    request.session.get("USER_EMAIL").map { sessionVal =>
+      Redirect(routes.UserArea.home)
+    }.getOrElse {
+      Ok(views.html.login(loginForm))
+    }
+  }
+
+  def login = Action.async { implicit request =>
+    // handling the login form
+    Future(
+      loginForm.bindFromRequest
+      ).map(
+        _.fold(
+          errorForm => BadRequest(views.html.login(errorForm)),
+          user => Redirect(routes.UserArea.home).withSession("USER_EMAIL" -> user.email)
+        )
+      )
+  }
+
+  def logout = Action {
+    Redirect(routes.UserArea.login).withNewSession
+  }
+
+  val loginForm = Form(
+    mapping(
+      "email" -> email
+      )(User.apply)(User.unapply).verifying(
+        "Invalid email", 
+        User.auth(_)
+      )
+    )
+
+}
